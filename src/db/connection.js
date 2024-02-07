df6cedb8c0881524f99152d86812eb4756ab2acd
vimalprakash404 <vimalprakash332@gmail.com>
2024-02-07 17:53:02 +0530

    data added

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://localhost:27017/myDatabase', {
+mongoose.connect('mongodb://localhost:27017/topTen', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
