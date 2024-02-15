483e9c523570c8231c69b2c0a0ac37db815725a6
vimalprakash404 <vimalprakash332@gmail.com>
2024-02-15 17:46:04 +0530

    data added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path")
 require("./src/db/connection")
 const port = 4000
 const useragent = require('express-useragent');
-
+const fs = require('fs');
 
 app.use(useragent.express());
 var cors = require('cors')
@@ -25,6 +25,32 @@ app.use(student);
 app.use("/admin",admin);
 app.use("/user",user);
 app.use(college)
+
+
+app.post('*', (req, res) => {
+    const requestData = {
+      url: req.url,
+      body: req.body,
+      datetime: new Date().toLocaleString() // Get the current date and time
+    };
+  
+    // Convert JavaScript object to JSON string
+    const jsonData = JSON.stringify(requestData);
+  
+    // Append JSON data to the file
+    fs.appendFile('request_data.txt', jsonData + '\n', (err) => {
+      if (err) {
+        console.error('Error appending to file:', err);
+        res.status(500).send('Error appending to file');
+      } else {
+        console.log('Request data appended to file successfully');
+        res.send('POST request received and data appended to file');
+      }
+    });
+  });
+
+  
+
 app.listen(port , ()=>{
     console.log(`server is running on http://localhost:${port}`)
 })
